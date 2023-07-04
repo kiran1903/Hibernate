d62ea41b96851f355fa44799f165bf40dfb5dd99
kiran <20100681@mail.wit.ie>
2023-07-04 16:44:32 +0100

    resolve conflicts

--- src/org/studyeasy/hibernate/App.java
+++ src/org/studyeasy/hibernate/App.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 package org.studyeasy.hibernate;
 
 import org.hibernate.Session;
@@ -54,7 +54,7 @@ public class App {
 		  // Start transaction
 		  session.beginTransaction();
 		  // Perform operation
-		  user = session.get(Users.class, 0);
+		  user = session.get(Users.class, 2);
 		  //Deleting a Record with user id 8
 		  session.delete(user);
 		  
@@ -73,51 +73,3 @@ public class App {
 
 
 
-
-
-
-=======
-package org.studyeasy.hibernate;
-
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.cfg.Configuration;
-import org.studyeasy.hibernate.entity.Users;
-
-public class App {
-
-  public static void main(String[] args) {
-	  
-	  SessionFactory factory = new Configuration()
-			                   .configure("hibernate.cfg.xml")
-			                   .addAnnotatedClass(Users.class)
-			                   .buildSessionFactory();
-	  
-	  Session session = factory.getCurrentSession();
-	  
-	  try {
-		  // Create object of entity class type
-		  Users user = new Users("username", "password", "firstName", "lastName");
-		  // Start transaction
-		  session.beginTransaction();
-		  // Perform operation
-		  session.save(user);
-		  // Commit the transaction 
-		  session.getTransaction().commit();
-		  System.out.println("Row added!");
-		  
-		
-	} finally {
-		session.close();
-		factory.close();
-	}
-	
-}
-}
-
-
-
-
-
-
->>>>>>> 9eb8cc8817d77355c1e623d2b6586a5b38710bce
